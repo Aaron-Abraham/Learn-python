3011c4f5dc9efb717868b7977e486fc9ee804fe7
Aaron-Abraham <75871595+Aaron-Abraham@users.noreply.github.com>
2023-10-19 19:55:45 +0530

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,4 @@
+print("First program-python using print() function #re-learning the basics")
+print("It is declared like this:")
+print("print('what to print always decides with you :) #have fun coding -w- ...')")
+
